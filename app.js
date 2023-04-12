c9a6ed9605aa3895b73d2813cb36b8c06a3512df
rominaascurra <rominaascurra4887@gmail.com>
2023-04-12 12:20:39 -0300

    Obteniendo datos del form

--- app.js
+++ app.js
@@ -37,4 +37,9 @@ class Datos{
 //agregar evento submit al formulario
 document.getElementById('libroForm').addEventListener('submit',(e) => {
     e.preventDefault();
+     //traer valores de los campos
+     const titulo = document.getElementById('titulo').value;
+     const autor = document.getElementById('autor').value;
+     const isbn = document.getElementById('isbn').value;
+     document.getElementById('libroForm').reset();
 });
